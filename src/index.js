5af8b51ecb0881a281309bd0f14dbcb4bd6c2d64
Ruslan Gusseinov <r.gusseinov@mail.ru>
2020-04-15 01:22:22 +0600

    added template and li items

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/* innerHTML
+textContent
+innerText
+
+ */
+
+console.log('--- BEFORE ---');
+let r1 = cardAction.innerText;
+console.log(r1);
+
+/* 
+cardAction.innerText = '<h1> The heading </h1>';
+
+console.log('--- AFTER ---');
+let r2 = cardAction.innerText;
+console.log(r2); */
