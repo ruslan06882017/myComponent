ebaaa143ca162d61e0bfb84271b29890c2879c8a
Ruslan Gusseinov <r.gusseinov@mail.ru>
2020-04-17 00:11:38 +0600

    Task 1

--- src/index.js
+++ src/index.js
@@ -1,16 +1,48 @@
-/* innerHTML
-textContent
-innerText
+let btn = document.getElementById("add-task");
+let table = document.getElementById("action-plan-table");
 
- */
+let actionPlan = document.getElementById("actionPlan").value;
+let assignedTo = document.getElementById("assignedTo");
+let selectedOption = assignedTo.options[assignedTo.selectedIndex].value;
+let deadline = document.getElementById("deadline").value;
 
-console.log('--- BEFORE ---');
-let r1 = cardAction.innerText;
-console.log(r1);
+/* console.log('---VALUES---');
+console.log(actionPlan, selectedOption, deadline); */
 
-/* 
-cardAction.innerText = '<h1> The heading </h1>';
+btn.addEventListener("click", function(e) { 
+    e.preventDefault();
 
-console.log('--- AFTER ---');
-let r2 = cardAction.innerText;
-console.log(r2); */
+    console.log(actionPlan.value);
+
+    let tr = document.createElement("tr");
+
+    let tdId = document.createElement("td");
+    tdId.appendChild(document.createTextNode("1"));
+    tr.appendChild(tdId);
+
+    let tdActionPlan = document.createElement("td");
+    tdActionPlan.appendChild(document.createTextNode(actionPlan));
+    tr.appendChild(tdActionPlan);
+
+    let tdAssignedTo = document.createElement("td");
+    tdAssignedTo.appendChild(document.createTextNode(selectedOption));
+    tr.appendChild(tdAssignedTo);
+
+    let tdDeadline = document.createElement("td");
+    tdDeadline.appendChild(document.createTextNode(deadline));
+    tr.appendChild(tdDeadline);
+
+    let tdAction = document.createElement("td");
+    let removeAction = document.createElement("a");
+    removeAction.innerHTML = '<i class="fa fa-remove" style="font-size:24px"></i>';
+
+    tdAction.appendChild(removeAction);
+    tr.appendChild(tdAction);
+
+    table.appendChild(tr);
+
+});
+
+document.addEventListener("click", function(e){
+    //console.log(`Clicked on ${e.target.tagName}`);
+})
