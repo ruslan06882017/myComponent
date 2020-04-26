9658d566ee0802e31ee6cf561d9e81b4e9c7169d
Ruslan Gusseinov <r.gusseinov@mail.ru>
2020-04-26 10:22:46 +0600

    easyHTTP function

---
+++ src/components/easyHTTP.js
@@ -0,0 +1,37 @@
+// Create EASY HTTP function
+ function easyHTTP(){
+    this.http = new XMLHttpRequest();
+ }
+
+ // GET Request
+ easyHTTP.prototype.get = function(url, callback){
+    this.http.open('GET', url, true);
+    let self = this;
+    this.http.onload = function(){
+        if (self.http.status === 200){
+            callback(null, self.http.responseText);
+        } else {
+            callback('Error: ', self.http.status);
+        }
+    }
+
+    this.http.send();
+ }
+
+
+ // POST Request
+
+ easyHTTP.prototype.post = function(url, data, callback){
+    this.http.open('POST', url, true);
+    this.http.setRequestHeader('Content-type', 'application/json');
+    let self = this;
+    this.http.onload = function(){
+        callback(null, self.http.responseText);
+    }
+
+    this.http.send(JSON.stringify(data));
+ }
+
+ 
+
+ export default easyHTTP;
--- src/index.js
+++ src/index.js
@@ -1,37 +1,23 @@
-document.getElementById('submit').addEventListener('click', getData);
+import easyHTTP from './components/easyHTTP'
 
-
-  //let result = document.getElementById('info');
-function getData(e){
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts', true);
-  xhr.onprogress = function() {
-    alert('Loading, please wait....');
+/* const http = new easyHTTP();
+http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts){
+  if (err){
+    console.log(err);
+  } else {
+    console.log(posts);
   }
+});  */
 
-
-  xhr.onload = function(){
-    alert('Loaded');
-    if (this.status == 200){
-      const data = JSON.parse(this.responseText);
-      const div = document.getElementById('info');
-      let result = '';
-      data.forEach(item => {
-        result += `
-          <ul>
-            <li> ${item.userId} </li>
-            <li> ${item.id} </li>
-            <li> ${item.title} </li>
-            <li> ${item.body} </li>
-          </ul>
-          <hr>`;
-
-        div.innerHTML = result;
-      });
-
-    }
+const http = new easyHTTP();
+const data = {
+  title: 'Custom title',
+  body: 'Here the long text 2020'
+}
+http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, posts){
+  if (err){
+    console.log(err);
+  } else {
+    console.log(posts);
   }
- 
-  xhr.send();
-  e.preventDefault();
-};
+}); 
